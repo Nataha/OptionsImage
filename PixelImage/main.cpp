9820e13894d79848e53491be7d82096d1c967f70
Nataha <natahametal@mail.ru>
2013-01-30 22:45:12 +0200

    New main.cpp

--- PixelImage/main.cpp
+++ PixelImage/main.cpp
@@ -1,36 +1,60 @@
 #include <QtGui>
 #include <QApplication>
+
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+
+
 //#include "ui_mainwindow.h"
-#include <iostream>
+//#include <iostream>
 
-
- class Widget : public QWidget
+/**/
+class Widget : public QWidget
 {
- protected:
-     void paintEvent(QPaintEvent *)
- {
-     int x,y,z;
-     QImage img("pixel100.jpg");
-     //QSize size(width(), height());
+protected:
+    void paintEvent(QPaintEvent *)
+    {
+        char matrix[100][100][3];
+        QImage img("pixel100.jpg");
 
-     QColor color;
-     color.getRgb(&x, &y, &z);
-     QRgb rgb = qRgb(x, y, z);
-     img.setPixel(img.width(), img.height(), rgb);
-     char matrix[x][y][z];
+        QColor color;
 
+        for(int i = 0; i < img.width(); ++i)
+        {
+            for(int j = 0; j < img.height(); ++j)
+            {
+                //img.pixel(i, j);
+                int x, y, z;
+                QRgb rgb = img.pixel(i,j);
+                x = qRed(rgb);
+                y = qGreen(rgb);
+                z = qBlue(rgb);
+                matrix[i][j][0] = x;
+                matrix[i][j][1] = y;
+                matrix[i][j][2] = z;
+                color.getRgb(&x, &y, &z);
+            }
+        }
 
+        QFile file("img.txt");
+        file.open(QIODevice::WriteOnly);
+        QTextStream out(&file);
+        for (int i = 0; i < img.width(); i++)
+        {
+            for (int j = 0; j < img.height();j++)
+            {
+                out << matrix[i][j][0];
+                out << matrix[i][j][1];
+                out << matrix[i][j][2];
+            }
+        }
+        //out.flush();
 
-     QFile file("img.txt");
-     file.open(QIODevice::WriteOnly);
-     QTextStream out(&file);
-     out << matrix;
-     //out.write(matrix);
-     file.close();
-
- }
+        //file.write(matrix);
+        file.close();
+    }
 };
-
+/**/
 int main(int argc, char** argv )
 {
     QApplication a(argc, argv);
