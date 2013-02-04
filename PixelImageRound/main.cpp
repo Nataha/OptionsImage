65d9e28603c4298f1f3707a557e1553fab6f4bce
Nataha <natahametal@mail.ru>
2013-02-04 20:19:21 +0200

    Add cycle

--- PixelImageRound/main.cpp
+++ PixelImageRound/main.cpp
@@ -9,13 +9,13 @@
 /**/
 class Widget : public QWidget
 {
-/*    Widget(QString line)
+    /*Widget()
     {
-    }
+    }*/
 private:
     QString line;
     unsigned char matrix[100][100][3];
-    int width, height;*/
+    int width, height;
 public:
     void ReadFile()
     {
@@ -28,6 +28,14 @@ public:
             line.remove(QRegExp("[],"));
             result.append(line);
             line = file.readAll();
+        }
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                QImage img;
+                img.setPixel(i, j, qRgb(matrix[i][j][0], matrix[i][j][1], matrix[i][j][2]));
+            }
         }
         qDebug() << result;
         /*QString alldata = QString(file.readAll());
