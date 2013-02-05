61344d9f3b7012eea6b86c510fe350111d2a62ed
Nataha <natahametal@mail.ru>
2013-02-05 10:40:48 +0200

    A correct use of the transposition

--- PixelImage/main.cpp
+++ PixelImage/main.cpp
@@ -13,16 +13,10 @@ class Widget : public QWidget
 public:
     Widget()
     {
-<<<<<<< HEAD
         img.load("pixel100.jpg");
         width = img.width();
         height = img.height();
 
-=======
-        QImage img("pixel100.jpg");
-        width = img.width();
-        height = img.height();
->>>>>>> c084c0c2c6f94701e997a5fa08f4f6486a6f6e1f
         for(int i = 0; i < width; ++i)
         {
             for(int j = 0; j < height; ++j)
@@ -43,7 +37,6 @@ public:
 private:
     unsigned char matrix[100][100][3];
     int width, height;
-<<<<<<< HEAD
     QImage img;
     //matrix transposition
 public:
@@ -53,6 +46,7 @@ public:
         {
             for(int j = i; j < height; j++)
             {
+                unsigned char matrix2[100][100][3];
                 char r, g, b;
                 r = matrix[i][j][0];
                 g = matrix[i][j][1];
@@ -60,26 +54,31 @@ public:
                 matrix[i][j][0] = matrix[j][i][0];
                 matrix[i][j][1] = matrix[j][i][1];
                 matrix[i][j][2] = matrix[j][i][2];
-                matrix[j][i][0] = r;
+                /*matrix[j][i][0] = r;
                 matrix[j][i][1] = g;
-                matrix[j][i][2] = b;
+                matrix[j][i][2] = b;*/
+
+                matrix2[j][i][0] = r;
+                matrix2[j][i][1] = g;
+                matrix2[j][i][2] = b;
+
+                matrix[j][i][0] = matrix2[j][i][0];
+                matrix[j][i][1] = matrix2[j][i][1];
+                matrix[j][i][2] = matrix2[j][i][2];
+
                 //img.setPixel(i, j, QColor::fromRgb(matrix[j][i][0],matrix[j][i][1],matrix[j][i][2]).argb);
                 ///ошибка: 'class QColor' has no member named 'argb'
-                img.setPixel(i, j, qRgb(matrix[j][i][0],matrix[j][i][1],matrix[j][i][2]));//QImage::Format_ARGB32
-                //img.fill(qRgb(matrix[j][i][0],matrix[j][i][1],matrix[j][i][2]));
+                img.setPixel(i, j, qRgb(matrix[j][i][0],matrix[j][i][1],matrix[j][i][2]));
             }
         }
     }
 
-=======
->>>>>>> c084c0c2c6f94701e997a5fa08f4f6486a6f6e1f
 public:
     void WriteFile()
     {
         QFile file("img.txt");
         file.open(QIODevice::WriteOnly);
         bool f;
-<<<<<<< HEAD
 
         for (int i = 0; i < width; i++)
         {
@@ -98,19 +97,6 @@ public:
                                        (int)matrix[i][j][0]) + "," + QString::number(
                                        (int)matrix[i][j][1]) + "," + QString::number(
                                        (int)matrix[i][j][2]) + "]").toUtf8());
-=======
-        for (int i = 0; i < width; i++)
-        {
-            f = false;
-            for (int j = 0; j < height;j++)
-            {
-                if (f) {
-                    file.write(",");
-                } else {
-                    f = true;
-                }
-                file.write(QString("["+QString::number((int)matrix[i][j][0])+","+QString::number((int)matrix[i][j][1])+","+QString::number((int)matrix[i][j][2])+"]").toUtf8());
->>>>>>> c084c0c2c6f94701e997a5fa08f4f6486a6f6e1f
             }
             file.write("\n");
         }
@@ -123,15 +109,6 @@ public:
 /**/
 int main(int argc, char** argv )
 {
-<<<<<<< HEAD
-=======
-    /*QFile file;
-    file.open(stdin, QFile::ReadOnly);
-
-    ReadFile();
-
-    return 0;*/
->>>>>>> c084c0c2c6f94701e997a5fa08f4f6486a6f6e1f
     QApplication a(argc, argv);
     Widget widget;
 
@@ -141,3 +118,4 @@ int main(int argc, char** argv )
 
     return a.exec();
 }
+/**/
